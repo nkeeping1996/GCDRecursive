eff7d76f83379a870d6cc7ec348fea17688ad578
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-21 09:28:36 -0400

    Recursive GCD algorithml, using modular arithmatic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(GCDRecursive(40,70));
+    }
+
+    public static int GCDRecursive(int a, int b){
+        if(b==0){
+            return a;
+        }
+        else{
+            return GCDRecursive(b, a%b);
+        }
     }
 }
